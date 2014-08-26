575c598a5af711373706fa4370369778a7c5ac29
James Bender <James@JamesCBender.com>
2014-08-26 19:52:32 -0400

    finished demo one

--- AgularInAspNetMvc/SimpleModelBinding/Controllers/HomeController.cs
+++ AgularInAspNetMvc/SimpleModelBinding/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SimpleModelBinding.Models;
 
 namespace SimpleModelBinding.Controllers
 {
@@ -25,6 +26,33 @@ namespace SimpleModelBinding.Controllers
             return View();
         }
 
+        public ActionResult Basket()
+        {
+            return View();
+        }
+
+        public ActionResult SimpleForm()
+        {
+            return View();
+        }
+
+        public ActionResult WithServices()
+        {
+            return View();
+        }
+
+        public JsonResult GetPerson(int id)
+        {
+            var person = new Person {FirstName = "Bob", LastName = "Smith", UserName = "BSmith", Number = 3};
+
+            return Json(person, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public void SavePerson(Person person)
+        {
+            return;
+        }
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
